46ede08b46ed25348f1bf6ad25a6ec2ace6c720f
dativus <sn.sable005@gmail.com>
2023-11-19 23:37:24 +0900

    update components/bio.js - add links

--- src/components/bio.js
+++ src/components/bio.js
@@ -12,10 +12,10 @@ const Bio = () => {
     <div className="mybio">
       <p>
         <a href="/sunatter">😪</a>
-        😴
-        😪
-        😴
-        <a href="/sunalog">😪</a>
+        <a href="/sunalog">😴</a>
+        <a href="/searchmode">😪</a>
+        <a href="https://zenn.dev/dativus">😴</a>
+        <a href="https://scrapbox.io/suna-64354306">😪</a>
         …
       </p>
     </div>
